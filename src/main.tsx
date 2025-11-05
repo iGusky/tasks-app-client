6572359f515b3cf20803d8aeab85dc2850531e49
Gustavo Hernández <gustavohernandez.dev@gmail.com>
2025-11-04 21:17:25 -0600

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import './index.css'
+import SignIn from './pages/auth/SignIn.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <div className='max-width-full p-4 min-w-lvw min-h-lvh bg-slate-100'>
+      <Routes>
+        <Route path='/auth/sign-in' element={<SignIn />}/>
+      </Routes>
+    </div>
+  </BrowserRouter>,
+)
